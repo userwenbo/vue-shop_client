d167f7456149dcafa3908323bab69530ac10fdba
wenbo <1501931802@qq.com>
2019-09-22 18:54:10 +0800

    msg2

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -91,7 +91,7 @@
     </section>
         <section class="profile_my_order border-1px" v-if="user._id">
           <mt-button type="danger" style="width: 100%" @click="logout">退出登录</mt-button>
-        </section>
+        </section> 
   </section>
   </template>
 

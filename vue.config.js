786325c0837ea490a2e8bf2af68a442811d6acc7
wenbo <1501931802@qq.com>
2019-09-26 10:33:11 +0800

    bug1

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 // vue.config.js
 module.exports = {
   devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    open: true,
     proxy: {
       '/api': { // 匹配所有以 '/api'开头的请求路径
         target: 'http://localhost:4000', // 代理目标的基础路径
